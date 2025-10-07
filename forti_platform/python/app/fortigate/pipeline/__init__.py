12d8c7fd9f7a7647fdfb76430457335c1335a9b9
byronyeh22 <159673004+byronyeh22@users.noreply.github.com>
2025-10-07 14:06:49 +0800

    update

--- forti_platform/python/app/fortigate/pipeline/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-forti_pipeline_bp = Blueprint("forti_pipeline_bp", __name__, url_prefix="/fortigate")
-
-from . import routes
--- forti_platform/python/app/fortigate/tasks/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-forti_tasks_bp = Blueprint("forti_tasks_bp", __name__, url_prefix="/fortigate")
-
-from . import routes
-
